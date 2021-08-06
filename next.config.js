a43fc1371ba690b3a45c5470ece94ca9989255a9
Cedric Jud <cedric@apt.no>
2021-08-06 17:10:07 +0200

    Disable next-offline plugin

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const withOffline = require('next-offline')
+// const withOffline = require('next-offline')
 
 const nextConfig = {
   webpack(config, { webpack, dev, isServer }) {
@@ -50,39 +50,39 @@ if (process.env.EXPORT !== 'true') {
   }
 }
 
-module.exports = plugins(
-  [
-    [
-      withOffline,
-      {
-        workboxOpts: {
-          swDest: process.env.NEXT_EXPORT
-            ? 'service-worker.js'
-            : 'static/service-worker.js',
-          runtimeCaching: [
-            {
-              urlPattern: /^https?.*/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'offlineCache',
-                expiration: {
-                  maxEntries: 200,
-                },
-              },
-            },
-          ],
-        },
-        async rewrites() {
-          return [
-            {
-              source: '/service-worker.js',
-              destination: '/_next/static/service-worker.js',
-            },
-          ]
-        },
-      },
-    ],
-    withBundleAnalyzer,
-  ],
-  nextConfig
-)
+// module.exports = plugins(
+//   [
+//     [
+//       withOffline,
+//       {
+//         workboxOpts: {
+//           swDest: process.env.NEXT_EXPORT
+//             ? 'service-worker.js'
+//             : 'static/service-worker.js',
+//           runtimeCaching: [
+//             {
+//               urlPattern: /^https?.*/,
+//               handler: 'NetworkFirst',
+//               options: {
+//                 cacheName: 'offlineCache',
+//                 expiration: {
+//                   maxEntries: 200,
+//                 },
+//               },
+//             },
+//           ],
+//         },
+//         async rewrites() {
+//           return [
+//             {
+//               source: '/service-worker.js',
+//               destination: '/_next/static/service-worker.js',
+//             },
+//           ]
+//         },
+//       },
+//     ],
+//     withBundleAnalyzer,
+//   ],
+//   nextConfig
+// )
